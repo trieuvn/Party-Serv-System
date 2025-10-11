ecadd3859b0fc0262343f6853c91abdf5264ffe1
Huy Nguyễn <huy59911@gmail.com>
2025-10-11 19:53:18 +0700

    StringUtils, EmailUtils, MapUtils

--- eParty/Utils/EmailUtils.cs
+++ eParty/Utils/EmailUtils.cs
@@ -1,11 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Mail;
 
 namespace eParty.Utils
 {
     public class EmailUtils
     {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+
+        public EmailUtils(string from, string to, string subject, string description)
+        {
+            From = from;
+            To = to;
+            Subject = subject;
+            Description = description;
+        }
+
+        public bool Send(string from, string to)
+        {
+            try
+            {
+                var mailMessage = new MailMessage(from, to, Subject, Description);
+                mailMessage.IsBodyHtml = true;
+
+                using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtpClient.Credentials = new NetworkCredential("your-email@gmail.com", "your-app-password");
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Send(mailMessage);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi gửi email: " + ex.Message);
+            }
+        }
+
+        public bool Send()
+        {
+            return Send(From, To);
+        }
     }
 }
