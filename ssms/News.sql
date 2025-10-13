8aa9da4106981828251e828d996c4da0cb7b4422
sang <sangtt222@uef.edu.vn>
2025-10-13 18:12:36 +0700

    SSMS

---
+++ ssms/News.sql
@@ -0,0 +1,9 @@
+﻿USE PartyServDb; SET NOCOUNT ON;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.News WHERE Subject=N'Ra mắt thực đơn Hải sản')
+INSERT dbo.News(Name,Subject,Description,Status,[User])
+VALUES (N'Thông báo',N'Ra mắt thực đơn Hải sản',N'Thêm nhiều món tôm mực tươi ngon cho mùa hè.',N'Active',N'admin');
+
+IF NOT EXISTS (SELECT 1 FROM dbo.News WHERE Subject=N'Ưu đãi tháng 10')
+INSERT dbo.News(Name,Subject,Description,Status,[User])
+VALUES (N'Tin khuyến mãi',N'Ưu đãi tháng 10',N'Giảm 15% cho đơn đặt tiệc trên 50 khách.',N'Active',N'admin');
---
+++ ssms/USER.sql
@@ -0,0 +1,22 @@
+﻿USE PartyServDb; SET NOCOUNT ON;
+
+-- Admin & Staff (TPH trong bảng User)
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='admin')
+INSERT dbo.[User](Username,Password,FirstName,LastName,Email,PhoneNumber,Role,Avatar,Discriminator,Salary)
+VALUES (N'admin',N'123456',N'Triệu',N'Nguyễn',N'admin@party.vn',N'0909000000',N'Admin',NULL,N'Staff',15000000);
+
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='staff.hcm')
+INSERT dbo.[User] VALUES (N'staff.hcm',N'123456',N'Linh',N'Phạm',N'staff.hcm@party.vn',N'0909000001',N'Staff',NULL,N'Staff',12000000);
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='staff.hn')
+INSERT dbo.[User] VALUES (N'staff.hn',N'123456',N'Nam',N'Ngô',N'staff.hn@party.vn',N'0909000002',N'Staff',NULL,N'Staff',11000000);
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='staff.dn')
+INSERT dbo.[User] VALUES (N'staff.dn',N'123456',N'Hương',N'Nguyễn',N'staff.dn@party.vn',N'0909000003',N'Staff',NULL,N'Staff',10000000);
+
+-- Khách hàng
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='user.tuan')
+INSERT dbo.[User](Username,Password,FirstName,LastName,Email,PhoneNumber,Role,Avatar,Discriminator,Salary)
+VALUES (N'user.tuan',N'123456',N'Tuấn',N'Phạm',N'tuan@party.vn',N'0901234567',N'User',NULL,N'User',NULL);
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='user.mai')
+INSERT dbo.[User] VALUES (N'user.mai',N'123456',N'Mai',N'Đỗ',N'mai@party.vn',N'0902345678',N'User',NULL,N'User',NULL);
+IF NOT EXISTS (SELECT 1 FROM dbo.[User] WHERE Username='user.thao')
+INSERT dbo.[User] VALUES (N'user.thao',N'123456',N'Thảo',N'Lê',N'thao@party.vn',N'0903456789',N'User',NULL,N'User',NULL);
