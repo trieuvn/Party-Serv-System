15b9edefb55e5d2bfd647c4d1efb7b152ca1ceb7
trieuvn <trieu123ok@gmail.com>
2025-10-27 17:04:14 +0700

    format currency

---
+++ eParty/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eParty.Models;
+
+namespace eParty.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public List<Menu> Menus { get; set; }
+        public List<MenuDetailDto> menuDetails { get; set; }
+        public List<Provider> Providers { get; set; }
+        public List<FoodDto> DtoFoods { get; set; }
+        public List<Food> Foods { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+        public List<SystemUser> RoleUsers { get; set; }
+        public String TotalCost { get; set; }
+    }
+}
---
+++ ssms/Category.sql
@@ -0,0 +1,8 @@
+﻿USE PartyServDb;
+SET NOCOUNT ON;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Category WHERE Name=N'Món khai vị')  INSERT dbo.Category(Name) VALUES (N'Món khai vị');
+IF NOT EXISTS (SELECT 1 FROM dbo.Category WHERE Name=N'Món chính')    INSERT dbo.Category(Name) VALUES (N'Món chính');
+IF NOT EXISTS (SELECT 1 FROM dbo.Category WHERE Name=N'Canh/Lẩu')     INSERT dbo.Category(Name) VALUES (N'Canh/Lẩu');
+IF NOT EXISTS (SELECT 1 FROM dbo.Category WHERE Name=N'Tráng miệng')  INSERT dbo.Category(Name) VALUES (N'Tráng miệng');
+IF NOT EXISTS (SELECT 1 FROM dbo.Category WHERE Name=N'Đồ uống')      INSERT dbo.Category(Name) VALUES (N'Đồ uống');
