827f6d424dc3bd3f75a1247cb8e49b24ad5728f8
trieuvn <trieu123ok@gmail.com>
2025-10-28 19:54:17 +0700

    hoan thanh 80%

--- eParty/Models/SystemUser.cs
+++ eParty/Models/SystemUser.cs
@@ -19,6 +19,9 @@ namespace eParty.Models
         [StringLength(50)] public string Email { get; set; }
         [StringLength(50)] public string PhoneNumber { get; set; }
         [StringLength(20)] public string Role { get; set; }
+        public int? Salary { get; set; }
+
+        public virtual ICollection<StaffParty> StaffParties { get; set; }
 
         public virtual ICollection<Party> Parties { get; set; }
         public virtual ICollection<UserDiscount> UserDiscounts { get; set; }
