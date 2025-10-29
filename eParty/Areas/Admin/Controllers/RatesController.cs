3b8b10e3809cd7364ef8aba3d5762ca8aff46d2d
Huy Nguyễn <huy59911@gmail.com>
2025-10-30 03:06:20 +0700

    View MenuDetail, Partner, Party, PrinceHistory, Rate, StaffParty, UserDiscount

---
+++ eParty/Areas/Admin/Controllers/RatesController.cs
@@ -0,0 +1,143 @@
+﻿using eParty.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace eParty.Areas.Admin.Controllers
+{
+    public class RatesController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Admin/Rates
+        public ActionResult Index()
+        {
+            var rates = db.Rates
+                .Include(r => r.PartyRef)
+                .Include(r => r.UserRef)
+                .ToList();
+            return View(rates);
+        }
+
+        // GET: Admin/Rates/Details
+        public ActionResult Details(string user, int? party)
+        {
+            if (string.IsNullOrEmpty(user) || party == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var rate = db.Rates
+                .Include(r => r.PartyRef)
+                .Include(r => r.UserRef)
+                .AsNoTracking()
+                .FirstOrDefault(r => r.User == user && r.Party == party);
+
+            if (rate == null)
+                return HttpNotFound();
+
+            return View(rate);
+        }
+
+        // GET: Admin/Rates/Create
+        public ActionResult Create()
+        {
+            ViewBag.Party = new SelectList(db.Parties, "Id", "Name");
+            ViewBag.User = new SelectList(db.SystemUsers, "Username", "Username");
+            return View();
+        }
+
+        // POST: Admin/Rates/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "User,Party,Stars,Comment")] Rate rate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rates.Add(rate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Party = new SelectList(db.Parties, "Id", "Name", rate.Party);
+            ViewBag.User = new SelectList(db.SystemUsers, "Username", "Username", rate.User);
+            return View(rate);
+        }
+
+        // GET: Admin/Rates/Edit
+        public ActionResult Edit(string user, int? party)
+        {
+            if (string.IsNullOrEmpty(user) || party == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var rate = db.Rates
+                .Include(r => r.PartyRef)
+                .Include(r => r.UserRef)
+                .FirstOrDefault(r => r.User == user && r.Party == party);
+
+            if (rate == null)
+                return HttpNotFound();
+
+            ViewBag.Party = new SelectList(db.Parties, "Id", "Name", rate.Party);
+            ViewBag.User = new SelectList(db.SystemUsers, "Username", "Username", rate.User);
+            return View(rate);
+        }
+
+
+        // POST: Admin/Rates/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "User,Party,Stars,Comment")] Rate rate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Party = new SelectList(db.Parties, "Id", "Name", rate.Party);
+            ViewBag.User = new SelectList(db.SystemUsers, "Username", "Username", rate.User);
+            return View(rate);
+        }
+
+        // GET: Admin/Rates/Delete
+        public ActionResult Delete(string user, int? party)
+        {
+            if (string.IsNullOrEmpty(user) || party == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var rate = db.Rates
+                .Include(r => r.PartyRef)
+                .Include(r => r.UserRef)
+                .FirstOrDefault(r => r.User == user && r.Party == party);
+
+            if (rate == null)
+                return HttpNotFound();
+
+            return View(rate);
+        }
+
+        // POST: Admin/Rates/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string user, int party)
+        {
+            var rate = db.Rates.FirstOrDefault(r => r.User == user && r.Party == party);
+            if (rate != null)
+            {
+                db.Rates.Remove(rate);
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "Rating deleted successfully!";
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eParty/Areas/Admin/Controllers/UserDiscountsController.cs
@@ -0,0 +1,143 @@
+﻿using eParty.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace eParty.Areas.Admin.Controllers
+{
+    public class UserDiscountsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Admin/UserDiscounts
+        public ActionResult Index()
+        {
+            var userDiscounts = db.UserDiscounts
+                .Include(u => u.UserRef)
+                .Include(u => u.DiscountRef);
+            return View(userDiscounts.ToList());
+        }
+
+        // GET: Admin/UserDiscounts/Details
+        public ActionResult Details(string user, int discount)
+        {
+            if (string.IsNullOrEmpty(user))
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var userDiscount = db.UserDiscounts
+                .Include(u => u.UserRef)
+                .Include(u => u.DiscountRef)
+                .FirstOrDefault(u => u.User == user && u.Discount == discount);
+
+            if (userDiscount == null)
+                return HttpNotFound();
+
+            return View(userDiscount);
+        }
+
+        // GET: Admin/UserDiscounts/Create
+        public ActionResult Create()
+        {
+            ViewBag.Discount = new SelectList(db.Discounts, "Id", "CouponCode");
+            ViewBag.User = new SelectList(db.SystemUsers, "Username", "Username");
+            return View();
+        }
+
+        // POST: Admin/UserDiscounts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "User,Discount,Amount")] UserDiscount userDiscount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserDiscounts.Add(userDiscount);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Discount = new SelectList(db.Discounts, "Id", "CouponCode", userDiscount.Discount);
+            ViewBag.User = new SelectList(db.SystemUsers, "Username", "Username", userDiscount.User);
+            return View(userDiscount);
+        }
+
+        // GET: Admin/UserDiscounts/Edit
+        public ActionResult Edit(string user, int discount)
+        {
+            if (string.IsNullOrEmpty(user))
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var userDiscount = db.UserDiscounts
+                .Include(u => u.UserRef)
+                .Include(u => u.DiscountRef)
+                .FirstOrDefault(u => u.User == user && u.Discount == discount);
+
+            if (userDiscount == null)
+                return HttpNotFound();
+
+            return View(userDiscount);
+        }
+
+        // POST: Admin/UserDiscounts/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserDiscount model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDiscount = db.UserDiscounts
+                    .FirstOrDefault(u => u.User == model.User && u.Discount == model.Discount);
+
+                if (userDiscount != null)
+                {
+                    userDiscount.Amount = model.Amount;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        // GET: Admin/UserDiscounts/Delete
+        public ActionResult Delete(string user, int discount)
+        {
+            if (string.IsNullOrEmpty(user))
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var userDiscount = db.UserDiscounts
+                .Include(u => u.UserRef)
+                .Include(u => u.DiscountRef)
+                .FirstOrDefault(u => u.User == user && u.Discount == discount);
+
+            if (userDiscount == null)
+                return HttpNotFound();
+
+            return View(userDiscount);
+        }
+
+        // POST: Admin/UserDiscounts/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string user, int discount)
+        {
+            var userDiscount = db.UserDiscounts
+                .FirstOrDefault(u => u.User == user && u.Discount == discount);
+
+            if (userDiscount != null)
+            {
+                db.UserDiscounts.Remove(userDiscount);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
